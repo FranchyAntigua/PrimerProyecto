9c99357fd9aaffb70ad4b0f2ac6f7b2a06f31aad
FranchyAntigua <Franchy_antigua@ucne.edu.do>
2019-05-30 16:18:50 -0400

    Subiendo proyecto

---
+++ BLL/ProductosBLL.cs
@@ -0,0 +1,120 @@
+﻿using PrimerProyecto.DAL;
+using PrimerProyecto.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto.BLL
+{
+    public class ProductosBLL
+    {
+        public static bool Guardar(Productos producto)
+        {
+            bool paso = false;
+
+            Contexto contexto = new Contexto();
+            try
+            {
+                if (contexto.Productos.Add(producto) != null)
+                {
+                    contexto.SaveChanges();
+                    paso = true;
+                }
+                contexto.Dispose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+
+        public static bool Modificar(Productos producto)
+        {
+            bool paso = false;
+
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Entry(producto).State = EntityState.Modified;
+                if (contexto.SaveChanges() > 0)
+                {
+                    paso = true;
+                }
+                contexto.Dispose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+
+            Contexto contexto = new Contexto();
+            try
+            {
+                Productos producto = contexto.Productos.Find(id);
+
+                contexto.Productos.Remove(producto);
+
+                if (contexto.SaveChanges() > 0)
+                {
+                    paso = true;
+                }
+                contexto.Dispose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+
+        public static Productos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Productos producto = new Productos();
+            try
+            {
+                producto = contexto.Productos.Find(id);
+                contexto.Dispose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return producto;
+        }
+
+
+        public static List<Productos> GetList(Expression<Func<Productos, bool>> expression)
+        {
+            List<Productos> productos = new List<Productos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                productos = contexto.Productos.Where(expression).ToList();
+                contexto.Dispose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return productos;
+        }
+
+    }
+}
---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using PrimerProyecto.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto.DAL
+{
+    public class Contexto : DbContext
+    {
+
+        public DbSet<Productos> Productos { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
